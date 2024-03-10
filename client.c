fb332fa08fdb10a1db2c5adf7e686c1f17c58cc2
tschan <jan.rendel@gmail.com>
2024-03-10 16:27:12 +0100

    Fixed Server and Client

--- client.c
+++ client.c
@@ -1,6 +1,98 @@
-#include<stdio.h>
+#define _XOPEN_SOURCE 700
 
-int main(void) {
-    printf("Hello, World\n");
-    return 0;
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <arpa/inet.h>
+#include <netdb.h> /* getprotobyname */
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define BUFF_SIZE 2048
+
+int main(int argc, char **argv) {
+    char buffer[BUFF_SIZE];
+    char protoname[] = "tcp";
+    struct protoent *protoent;
+    char *server_hostname = "127.0.0.1";
+    char *user_input = NULL;
+    in_addr_t in_addr;
+    in_addr_t server_addr;
+    int sockfd;
+    size_t getline_buffer = 0;
+    ssize_t nbytes_read, i, user_input_len;
+    struct hostent *hostent;
+    /* This is the struct used by INet addresses. */
+    struct sockaddr_in sockaddr_in;
+    unsigned short server_port = 6969u;
+
+    if (argc > 1) {
+        server_hostname = argv[1];
+        if (argc > 2) {
+            server_port = strtol(argv[2], NULL, 10);
+        }
+    }
+
+    /* Get socket. */
+    protoent = getprotobyname(protoname);
+    if (protoent == NULL) {
+        perror("getprotobyname");
+        exit(EXIT_FAILURE);
+    }
+    sockfd = socket(AF_INET, SOCK_STREAM, protoent->p_proto);
+    if (sockfd == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Prepare sockaddr_in. */
+    hostent = gethostbyname(server_hostname);
+    if (hostent == NULL) {
+        fprintf(stderr, "error: gethostbyname(\"%s\")\n", server_hostname);
+        exit(EXIT_FAILURE);
+    }
+    in_addr = inet_addr(inet_ntoa(*(struct in_addr*)*(hostent->h_addr_list)));
+    if (in_addr == (in_addr_t)-1) {
+        fprintf(stderr, "error: inet_addr(\"%s\")\n", *(hostent->h_addr_list));
+        exit(EXIT_FAILURE);
+    }
+    sockaddr_in.sin_addr.s_addr = in_addr;
+    sockaddr_in.sin_family = AF_INET;
+    sockaddr_in.sin_port = htons(server_port);
+
+    /* Do the actual connection. */
+    if (connect(sockfd, (struct sockaddr*)&sockaddr_in, sizeof(sockaddr_in)) == -1) {
+        perror("connect");
+        return EXIT_FAILURE;
+    }
+    while (1) {
+        fprintf(stderr, "enter string (empty to quit):\n");
+        user_input_len = getline(&user_input, &getline_buffer, stdin);
+        if (user_input_len == -1) {
+            perror("getline");
+            exit(EXIT_FAILURE);
+        }
+        if (user_input_len == 1) {
+            close(sockfd);
+            break;
+        }
+        if (write(sockfd, user_input, user_input_len) == -1) {
+            perror("write");
+            exit(EXIT_FAILURE);
+        }
+        while ((nbytes_read = read(sockfd, buffer, BUFF_SIZE)) > 0) {
+            write(STDOUT_FILENO, buffer, nbytes_read);
+            if (buffer[nbytes_read - 1] == '\n') {
+                fflush(stdout);
+                break;
+            }
+        }
+    }
+    free(user_input);
+
+    exit(EXIT_SUCCESS);
 }
--- server.c
+++ server.c
@@ -1,31 +1,88 @@
+#define _XOPEN_SOURCE 700
+
+#include <stdbool.h>
 #include <stdio.h>
-#include <sys/types.h>          /* See NOTES */
-#include <sys/socket.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include <arpa/inet.h>
+#include <netdb.h> /* getprotobyname */
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 #define BUFF_SIZE 2048
-#define READ_SIZE 64    
 
-char *buff;
+int main(int argc, char **argv) {
+    char buffer[BUFF_SIZE];
+    char *protoname = "tcp";
+    struct protoent *protoent;
+    int enable = 1;
+    int i;
+    int newline_found = 0;
+    int server_sockfd, client_sockfd;
+    socklen_t client_len;
+    ssize_t nbytes_read;
+    struct sockaddr_in client_address, server_address;
+    unsigned short server_port = 6969u;
 
-struct sockaddr {
-   sa_family_t sa_family;
-   char        sa_data[14];
-}
-
-int main(void) {
-    int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (!sockfd) {
-        fprintf(stderr, "Socket could not be opened.");
-        return -1;
+    if (argc > 1) {
+        server_port = strtol(argv[1], NULL, 10);
     }
-    buff = malloc(BUFF_SIZE);
-    if (!buff) {
-        fprintf(stderr, "Error while allocating memory.");
-        return -1;
+
+    protoent = getprotobyname(protoname);
+    if (protoent == NULL) {
+        perror("getprotobyname");
+        exit(EXIT_FAILURE);
     }
-    printf("Socket opened on fd: %d", sockfd); 
-    return 0;
 
+    server_sockfd = socket( AF_INET, SOCK_STREAM, protoent->p_proto);
+    if (server_sockfd == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
 
+    if (setsockopt(server_sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)) < 0) {
+        perror("setsockopt(SO_REUSEADDR) failed");
+        exit(EXIT_FAILURE);
+    }
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(server_port);
+    if (bind(
+            server_sockfd,
+            (struct sockaddr*)&server_address,
+            sizeof(server_address)
+        ) == -1
+    ) {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_sockfd, 5) == -1) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(stderr, "listening on port %d\n", server_port);
+
+    while (1) {
+        client_len = sizeof(client_address);
+        client_sockfd = accept(
+            server_sockfd,
+            (struct sockaddr*)&client_address,
+            &client_len
+        );
+        while ((nbytes_read = read(client_sockfd, buffer, BUFF_SIZE)) > 0) {
+            printf("received:\n");
+            write(STDOUT_FILENO, buffer, nbytes_read);
+            if (buffer[nbytes_read - 1] == '\n')
+                newline_found;
+            write(client_sockfd, buffer, nbytes_read);
+            if (newline_found)
+                break;
+        }
+        close(client_sockfd);
+    }
+    return EXIT_SUCCESS;
 }
